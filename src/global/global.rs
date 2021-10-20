7f2cc7274c2efe1fdbee4edce3e8f05072eefd86
nalanrola <nalanrola@gmail.com>
2021-10-20 08:54:21 +0800

    test excel

---
+++ src/global/global.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate lazy_static;
+
+extern crate config;
+
+use std::error::Error;
+use std::sync::RwLock;
+use config::Config;
+
+lazy_static! {
+	static ref SETTINGS: RwLock<Config> = RwLock::new(Config::default());
+}
+
+pub  fn init() -> Result<(), Box<dyn Error>> {
+	// Set property
+	SETTINGS.write()?.set("property", 42)?;
+	// Get property
+	println!("property: {}", SETTINGS.read()?.get::<i32>("property")?);
+	Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,44 @@
-use std::{thread, time::Duration};
+use office::{CellErrorType, DataType, Excel};
+// use ansi_term::Colour::Green;
+// use ansi_term::Colour::Red;
+// use std::{thread, time::Duration};
 
 fn main() {
-    ctrlc::set_handler(move || {
-        println!("received Ctrl+C!");
-    })
-    .expect("Error setting Ctrl-C handler");
+    // opens a new workbook
+    let path = format!("{}/Chat.xlsm", env!("CARGO_MANIFEST_DIR"));
+    // let path = std::path::Path::new("./Chat.xlsm");
+    let mut workbook = Excel::open(path).unwrap();
+    // Read whole worksheet data and provide some statistics
+    // if let Ok(range) = workbook.worksheet_range("Chat") {
+    //     let total_cells = range.get_size().0 * range.get_size().1;
+    //     let non_empty_cells: usize = range
+    //         .rows()
+    //         .map(|r| r.iter().filter(|cell| cell != &&DataType::Empty).count())
+    //         .sum();
+    //     println!(
+    //         "Found {} cells in 'Chat', including {} non empty cells",
+    //         total_cells, non_empty_cells
+    //     );
+    // }
 
-    // Following code does the actual work, and can be interrupted by pressing
-    // Ctrl-C. As an example: Let's wait a few seconds.
-    thread::sleep(Duration::from_secs(2));
-    println!("Hello world!!!");
+    let sheets = workbook.sheet_names().expect("Empty workbook");
+    for sheet_name in sheets {
+        if sheet_name.starts_with('_') {
+            continue;
+        }
+        println!("{}", &sheet_name);
+        let range = workbook.worksheet_range(&sheet_name).unwrap();
+        // let total_cells = range.get_size().0 * range.get_size().1;
+        // let non_empty_cells: usize = range
+            // .rows()
+            // .map(|r| r.iter().filter(|cell| cell != &&DataType::Empty).count())
+            // .sum();
+        if &sheet_name == "Chat" {
+            for c in range.rows() {
+                if c != DataType::Empty {
+                    println!("{:?}", &c);
+                }
+            }
+        }
+    }
 }
